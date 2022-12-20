afb7794d592c3cfcdd66ed4ce48f1a7693b890ac
Fra-Battaglia <francescobattaglia03@gmail.com>
2022-12-20 16:57:16 +0100

    exercise done

---
+++ js/script.js
@@ -0,0 +1,68 @@
+const { createApp } = Vue
+
+createApp({
+    data() {
+        return {
+            active_image: 0,
+            autoplay: null,
+            slides: [
+                {
+                    image: 'img/01.webp',
+                    title: 'Marvel\'s Spiderman Miles Morale',
+                    text: 'Experience the rise of Miles Morales as the new hero masters incredible, explosive new powers to become his own Spider-Man.',
+                }, 
+                {
+                    image: 'img/02.webp',
+                    title: 'Ratchet & Clank: Rift Apart',
+                    text: 'Go dimension-hopping with Ratchet and Clank as they take on an evil emperor from another reality.',
+                }, 
+                {
+                    image: 'img/03.webp',
+                    title: 'Fortnite',
+                    text: "Grab all of your friends and drop into Epic Games Fortnite, a massive 100 - player face - off that combines looting, crafting, shootouts and chaos.",
+                }, 
+                {
+                    image: 'img/04.webp',
+                    title: 'Stray',
+                    text: 'Lost, injured and alone, a stray cat must untangle an ancient mystery to escape a long-forgotten city',
+                }, 
+                {
+                    image: 'img/05.webp',
+                    title: "Marvel's Avengers",
+                    text: 'Marvel\'s Avengers is an epic, third-person, action-adventure game that combines an original, cinematic story with single-player and co-operative gameplay.',
+                }
+            ],
+        }
+    },
+    created() {
+        this.autoplay;
+    },
+    methods: {
+        change_image(slide_index) {
+            this.active_image = slide_index;
+        },
+
+        next() {
+            this.active_image++;
+            if (this.active_image > this.slides.length - 1) {
+                this.active_image = 0;
+            }
+        },
+
+        prev() {
+            this.active_image--;
+            if (this.active_image < 0) {
+                this.active_image = this.slides.length - 1;
+            }
+        },
+        autoscroll() {
+            this.autoplay = setInterval(() => {
+                this.next();
+            },3000)
+        },
+        stop_autoscroll() {
+            clearInterval(this.autoplay)
+            this.autoplay = null;
+        }
+    }
+}).mount('#app')
